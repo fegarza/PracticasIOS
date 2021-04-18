951c0e4b20fbdfe3d9eadcc13b07ac4477adcffc
Felipe Garza <fegarza@Felipes-MacBook-Pro.local>
2021-04-17 17:00:40 -0700

    proyecto iniciado

---
+++ Practicas/Shared/PracticasApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticasApp.swift
+//  Shared
+//
+//  Created by Felipe Garza on 17/04/21.
+//
+
+import SwiftUI
+
+@main
+struct PracticasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
